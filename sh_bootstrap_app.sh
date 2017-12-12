003ea34640c27e21428957d0fac25f4ef3cf57ac
chapeau <mou.idri@gmail.com>
2017-12-12 09:43:11 +0100

    Add files via upload

--- sh_bootstrap_app.sh
+++ sh_bootstrap_app.sh
@@ -16,28 +16,10 @@ export DB_USER=wordpress
 export DB_PWD=password
 
 
-#Fetch  website
-rm -rf /var/www/html/*
-mkdir /tmp/temp-website/
-cd /tmp/temp-website/
-rm -rf /tmp/temp-website/*
+#Make dirs
+mkdir /APP/
+cd /APP/
+rm -rf /APP/*
 
-git clone  https://github.com/MourIdri/front-end-mou-cust.git
-cp -R /tmp/temp-website/front-end-mou-cust/* /var/www/html/
-
-mkdir /var/www/html/cgi-bin/sessions_log
-chown -R www-data:www-data /var/www/html/
-mv /var/www/html/index.html /var/www/html/oldindex.htmlbkp
-chmod -R 755 /var/www/html/
-chmod a+rwx /var/www/html/cgi-bin/sessions_log
-chmod a+rwx /var/www/html/cgi-bin/
-
-cd /etc/apache2/
-rm /etc/apache2/apache2.conf
-wget https://rgcloudmouradgeneralpurp.blob.core.windows.net/exchangecontainermourad/etcapache2apache2.conf
-mv etcapache2apache2.conf apache2.conf
-
-cd /etc/apache2/conf-enabled/
-rm /etc/apache2/conf-enabled/serve-cgi-bin.conf
-wget https://rgcloudmouradgeneralpurp.blob.core.windows.net/exchangecontainermourad/etcapache2conf-enabledserve-cgi-bin.conf
-mv etcapache2conf-enabledserve-cgi-bin.conf serve-cgi-bin.conf
+git clone  https://github.com/MourIdri/back-end-app.git
+chmod -R 777 /APP/*
