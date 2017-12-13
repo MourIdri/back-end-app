ee5a0df19fb5a9fbf4c301dec0a78a51311f6a27
chapeau <mou.idri@gmail.com>
2017-12-13 17:56:06 +0100

    Add files via upload

--- sh_bootstrap_app.sh
+++ sh_bootstrap_app.sh
@@ -2,13 +2,14 @@
 apt-get update -y
 
 # install PHP and MySQL
-apt-get install git apache2 ifstat inetutils-traceroute traceroute telnet curl python python-pip python3 python3-pip libapache2-mod-python libapache2-mod-php php-mysql -y
+
+apt-get install git ifstat inetutils-traceroute traceroute telnet curl python python-pip python3 python3-pip libapache2-mod-python libapache2-mod-php php-mysql -y
 apt-get install mysql-client -y
 pip install requests flask ConfigParser mysql-connector-python flask-restful ast 
-pip3 install requests
+pip3 install requests 
 apt-get install php php-fpm php-mysql php-curl php-gd php-pear php-imagick php-imap php-mcrypt php-recode php-tidy php-xmlrpc php-curl php-gd php-intl php-json php-mbstring php-mcrypt php-xml php-zip -y
 
-sudo a2enmod cgi
+apt-get remove apache2
 #systemctl enable mariadb.service
 
 export DB_NAME=customersDB
@@ -24,4 +25,4 @@ rm -rf /APP/*
 
 git clone  https://github.com/MourIdri/back-end-app.git
 chmod -R 777 /APP/*
-python app.py 
+python /APP/back-end-app/app.py 
